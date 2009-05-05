2ad4613df55e7a86659d4483312014e3eded82d1
Eng.A.Abdalla <Eng.A.Abdalla@25fe762a-3319-11de-8c0b-2fbac4a05959>
2009-05-05 23:31:00 +0000

    Added Runnable and Thread, nothing much yet

---
+++ trunk/cppThreads/include/Runnable.h
@@ -0,0 +1,19 @@
+/*
+ * Thread.h
+ *
+ *  Created on: May 5, 2009
+ *      Author: Abd4llA
+ */
+
+#ifndef RUNNABLE_H_
+#define RUNNABLE_H_
+
+class Runnable{
+	private:
+
+	public:
+		void run() = 0;
+
+};
+
+#endif /* RUNNABLE_H_ */
