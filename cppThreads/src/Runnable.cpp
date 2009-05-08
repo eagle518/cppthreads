7dccc71a7f86cf1bf75d29f1848034fc9713e1b8
Eng.A.Abdalla <Eng.A.Abdalla@25fe762a-3319-11de-8c0b-2fbac4a05959>
2009-05-08 00:11:48 +0000

    Implemented Thread and Runnable * Full implementation of creating a Thread usecase. * Partial implementation of some Thread methods * Not tested yet

---
+++ cppThreads/src/Runnable.cpp
@@ -0,0 +1,16 @@
+/*
+ * Runnable.cpp
+ *
+ *  Created on: May 8, 2009
+ *      Author: Abd4llA
+ */
+#include<iostream>
+#include "Runnable.h"
+using namespace std;
+
+namespace cppthreads {
+
+	Runnable::~Runnable(){
+
+	}
+}
