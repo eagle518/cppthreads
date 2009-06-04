0d386fdb3affb87de2e7777d61eb63266631005e
Eng.A.Abdalla <Eng.A.Abdalla@25fe762a-3319-11de-8c0b-2fbac4a05959>
2009-06-04 22:08:03 +0000

    * The rest of the previous commit

---
+++ trunk/cppThreads/include/TimeOutException.h
@@ -0,0 +1,24 @@
+/*
+ * ThreadJoiningFailedException.h
+ *
+ *  Created on: May 18, 2009
+ *      Author: Abd4llA
+ */
+
+#ifndef TIMEOUTEXCEPTION_H_
+#define TIMEOUTEXCEPTION_H_
+
+#include<sys/types.h>
+
+namespace cppthreads {
+	class TimeOutException: public ThreadingException {
+		public:
+			TimeOutException(const char *msg, int32_t errorCode) : ThreadingException(msg,errorCode){
+
+			}
+			virtual ~TimeOutException() throw(){
+
+			}
+	};
+}
+#endif /* TIMEOUTEXCEPTION_H_ */
--- trunk/cppThreads/tests/ThreadTest.cpp
+++ trunk/cppThreads/tests/ThreadTest.cpp
@@ -11,7 +11,8 @@
 #include "Thread.h"
 #include "Runnable.h"
 #include "Mutex.h"
-#include "ThreadAlreadyStartedException.h"
+#include "AlreadyStartedException.h"
+#include "TimeOutException.h"
 
 using namespace cppthreads;
 using namespace std;
@@ -47,15 +48,27 @@ class RunnableObject : public Runnable {
 
 		}
 		void run(){
-			for (int i =0; i<100;i++) {
-//				cout << "Adding Element " << i + 1 << endl;
+			for (int i =0; i < 100; i++) {
 				elements_->addElement(i);
-//				usleep(100);
 			}
 
 		}
-		~RunnableObject(){
-			cout << "RunnableObject deleting: " << this << endl;
+	private:
+		SynchornizedList  *elements_;
+};
+
+class LongRunObj : public Runnable {
+	//A runnable that takes too long to finish
+	public:
+		LongRunObj(SynchornizedList *myList) : elements_(myList) {
+
+		}
+		void run(){
+			for (int i =0; i < 2; i++) {
+				elements_->addElement(i);
+				sleep(1);
+			}
+
 		}
 	private:
 		SynchornizedList  *elements_;
@@ -63,7 +76,6 @@ class RunnableObject : public Runnable {
 
 TEST(ThreadTest, ThreadsRun){
 	SynchornizedList *myList_;
-	Runnable * runnable;
 	Thread *thread1 ;
 	Thread *thread2;
 	Thread *thread3;
@@ -79,10 +91,11 @@ TEST(ThreadTest, ThreadsRun){
 	thread2->start();
 	ASSERT_FALSE(thread3->isRunning());
 	thread3->start();
-	cout  << "Joining Threads..." << endl;
+
 	thread1->join();
 	thread2->join();
 	thread3->join();
+
 	ASSERT_EQ(300, myList_->getSize());
 
 	delete thread1;
@@ -93,17 +106,14 @@ TEST(ThreadTest, ThreadsRun){
 }
 TEST(ThreadTest, ThreadsJoinable){
 	SynchornizedList *myList_;
-	Runnable * runnable;
 	Thread *thread1 ;
 
 	myList_ = new SynchornizedList();
 	thread1 = new Thread(new RunnableObject(myList_));
 
-	cout << "Starting Thread" << endl;
 	thread1->start();
-	cout << "Joining thread1" << endl;
-	thread1->join(3);
-	cout << "Joining thread2" << endl;
+	thread1->join();
+
 	ASSERT_FALSE(thread1->isRunning());
 	ASSERT_EQ(100, myList_->getSize());
 
@@ -111,8 +121,40 @@ TEST(ThreadTest, ThreadsJoinable){
 	delete myList_;
 
 }
+/**
+* For testing Thread.join we have the following test cases:
+* 	1-Normal join is called before the thread starts
+*   2-Normal join is called after the thread is started and before it finishes (Thread is running)
+*   3-Normal join is called after the thread finishes
+*  	4-Timed join is called before the thread starts
+*   5-Timed join is called after the thread is started and before it finishes (Thread is running)
+*   6-Timed join is called after the thread finishes
+*   7-Timed join times out cause thread didn't finish execution and timeout reached
+*
+* The following TEST cases represent the previous ones respectively
+*/
+
+TEST(ThreadTest, ThreadsJoinable1){
+	// Test of normal join is called before the thread starts
+
+	SynchornizedList *myList_;
+	Thread *thread1 ;
+
+	myList_ = new SynchornizedList();
+	thread1 = new Thread(new RunnableObject(myList_));
+
+	thread1->join();
+
+	ASSERT_FALSE(thread1->isRunning());
+	ASSERT_EQ(0, myList_->getSize());
+
+	delete thread1;
+	delete myList_;
+
+}
 
 TEST(ThreadTest, ThreadsJoinable2){
+//  Test of normal join is called after the thread is started and before it finishes (Thread is running)
 	SynchornizedList *myList_;
 	Runnable * runnable;
 	Thread *thread1 ;
@@ -120,13 +162,10 @@ TEST(ThreadTest, ThreadsJoinable2){
 	myList_ = new SynchornizedList();
 	thread1 = new Thread(new RunnableObject(myList_));
 
-	cout << "Starting Thread" << endl;
 	thread1->start();
-	cout << "Joining thread1" << endl;
 	// will make sure that you have the thread started already..
 	usleep(10);
-	thread1->join(3);
-	cout << "Joining thread2" << endl;
+	thread1->join();
 	ASSERT_FALSE(thread1->isRunning());
 	ASSERT_EQ(100, myList_->getSize());
 
@@ -136,40 +175,17 @@ TEST(ThreadTest, ThreadsJoinable2){
 }
 
 TEST(ThreadTest, ThreadsJoinable3){
+//  Test of normal join is called after the thread finishes
 	SynchornizedList *myList_;
-	Runnable * runnable;
 	Thread *thread1 ;
 
 	myList_ = new SynchornizedList();
 	thread1 = new Thread(new RunnableObject(myList_));
 
-	cout << "Starting Thread" << endl;
-	cout << "Joining thread1" << endl;
-	thread1->join(3);
 	thread1->start();
-	cout << "Joining thread2" << endl;
+	// will make sure that you have the thread started already..
+	usleep(20);
 	thread1->join();
-	ASSERT_EQ(100, myList_->getSize());
-
-	delete thread1;
-	delete myList_;
-
-}
-
-TEST(ThreadTest, ThreadsJoinable1){
-	SynchornizedList *myList_;
-	Runnable * runnable;
-	Thread *thread1 ;
-
-	myList_ = new SynchornizedList();
-	thread1 = new Thread(new RunnableObject(myList_));
-
-	cout << "Starting Thread" << endl;
-	thread1->start();
-	usleep(2000);
-	cout << "Joining thread1" << endl;
-	thread1->join(3);
-	cout << "Joining thread2" << endl;
 	ASSERT_FALSE(thread1->isRunning());
 	ASSERT_EQ(100, myList_->getSize());
 
@@ -177,25 +193,116 @@ TEST(ThreadTest, ThreadsJoinable1){
 	delete myList_;
 
 }
-TEST(ThreadTest, ThreadsRestartingFails){
+
+TEST(ThreadTest, ThreadsJoinable4){
+//  Test of timed join is called before the thread starts
 	SynchornizedList *myList_;
-	Runnable * runnable;
+	Runnable *runnable;
 	Thread *thread1 ;
-	Thread *thread2;
-	Thread *thread3;
 
 	myList_ = new SynchornizedList();
 	thread1 = new Thread(new RunnableObject(myList_));
-	thread2 = new Thread(new RunnableObject(myList_));
-	thread3 = new Thread(new RunnableObject(myList_));
+
+	thread1->join(3);
+
+	ASSERT_FALSE(thread1->isRunning());
+	ASSERT_EQ(0, myList_->getSize());
+
+	delete thread1;
+	delete myList_;
+
+}
+
+TEST(ThreadTest, ThreadsJoinable5){
+//  Test of timed join is called after the thread is started and before it finishes (Thread is running)
+	SynchornizedList *myList_;
+	Thread *thread1 ;
+
+	myList_ = new SynchornizedList();
+	thread1 = new Thread(new RunnableObject(myList_));
+
+	thread1->start();
+	// will make sure that you have the thread started already..
+	usleep(10);
+	thread1->join(3);
+	ASSERT_FALSE(thread1->isRunning());
+	ASSERT_EQ(100, myList_->getSize());
+
+	delete thread1;
+	delete myList_;
+
+}
+
+TEST(ThreadTest, ThreadsJoinable6){
+//  Test of timed join is called after the thread finishes
+	SynchornizedList *myList_;
+	Thread *thread1 ;
+
+	myList_ = new SynchornizedList();
+	thread1 = new Thread(new RunnableObject(myList_));
+
+	thread1->start();
+	// will make sure that you have the thread started already..
+	usleep(20);
+	thread1->join();
+	ASSERT_FALSE(thread1->isRunning());
+	ASSERT_EQ(100, myList_->getSize());
+
+	delete thread1;
+	delete myList_;
+
+}
+
+TEST(ThreadTest, ThreadsJoinable7){
+//  Test of timed join times out cause thread didn't finish execution and timeout reached
+	SynchornizedList *myList_;
+	Thread *thread1 ;
+	myList_ = new SynchornizedList();
+	thread1 = new Thread(new LongRunObj(myList_));
+
+	thread1->start();
+	/*struct timespec currentTime;
+	clock_gettime(CLOCK_REALTIME,&currentTime);
+	cout << "Current time " << currentTime.tv_sec << endl;*/
+	usleep(50);
+	ASSERT_THROW(thread1->join(1), TimeOutException);
+	/*clock_gettime(CLOCK_REALTIME,&currentTime);
+	cout << "Current time " << currentTime.tv_sec << endl;*/
+	ASSERT_TRUE(thread1->isRunning());
+	thread1->join();
+	delete thread1;
+	delete myList_;
+
+}
+TEST(ThreadTest, ThreadsStartAfterJoin){
+	SynchornizedList *myList_;
+	Thread *thread1 ;
+
+	myList_ = new SynchornizedList();
+	thread1 = new Thread(new RunnableObject(myList_));
+
+	thread1->join(3);
+	thread1->start();
+	thread1->join();
+	ASSERT_EQ(100, myList_->getSize());
+
+	delete thread1;
+	delete myList_;
+
+}
+
+TEST(ThreadTest, ThreadsRestartingFails){
+	SynchornizedList *myList_;
+	Thread *thread1 ;
+
+	myList_ = new SynchornizedList();
+	thread1 = new Thread(new RunnableObject(myList_));
 
 	thread1->start();
 	thread1->join();
-	ASSERT_THROW(thread1->start(), ThreadAlreadyStartedException);
+	ASSERT_THROW(thread1->start(), AlreadyStartedException);
 
 	delete thread1;
-	delete thread2;
-	delete thread3;
 	delete myList_;
 
 }
