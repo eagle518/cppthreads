87a795a933d7d99c077313a26bbcfa028d85bd04
Eng.A.Abdalla <Eng.A.Abdalla@25fe762a-3319-11de-8c0b-2fbac4a05959>
2009-06-08 22:14:05 +0000

    * Adds detached features for Thread * Adds Threads cancelation mechanism * Only one test case, more tests will be added

---
+++ cppThreads/include/DetachException.h
@@ -0,0 +1,26 @@
+/*
+ * DetachException.h
+ *
+ *  Created on: May 18, 2009
+ *      Author: Abd4llA
+ */
+
+#ifndef DETACHEXCEPTION_H_
+#define DETACHEXCEPTION_H_
+
+#include<sys/types.h>
+
+namespace cppthreads {
+	class DetachException: public ThreadingException {
+		public:
+			DetachException(const char *msg, int32_t errorCode) : ThreadingException(msg,errorCode){
+
+			}
+			virtual ~DetachException() throw(){
+
+			}
+	};
+}
+
+
+#endif /* DETACHEXCEPTION_H_ */
